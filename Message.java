15a518da090ba907d3f127730b10363a573a472c
peshmamtaib <md.taib21@gmail.com>
2023-06-18 18:13:17 +0530

    Added message file

---
+++ Message.java
@@ -0,0 +1,14 @@
+class Multiplication
+{
+        public static void main (String[]args)
+        {
+                int num1,num2,product=0;
+                num1=20;
+                num2=50;
+                product= num1*num2;
+                System.out.println("The Intialized value for num1 is :"+num1);
+                System.out.println("The Intialized value for num2 is :"+num2);
+                System.out.println("The product of two numbers is :" + product);
+        }
+}
+
