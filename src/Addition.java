3c8eb3366a2f9e967fb2954093da303c529048db
peshmamtaib <md.taib21@gmail.com>
2023-06-18 18:10:18 +0530

    Added  file that is Addition.java

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+class Addition
+{
+        public static void main (String[]args)
+        {
+                int num1,num2,sum=0;
+                num1=20;
+                num2=50;
+                sum= num1+num2;
+                System.out.println("The Intialized value for num1 is :"+num1);
+                System.out.println("The Intialized value for num2 is :"+num2);
+                System.out.println("The sum of two numbers is :" + sum);
+        }
+}
+
